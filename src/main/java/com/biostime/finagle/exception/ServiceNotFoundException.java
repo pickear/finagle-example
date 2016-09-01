0f9842b58dfef861dba4eaa88ba94766e369ec59
pickear <pickear@gmail.com>
2016-09-01 09:57:03 +0800

    添加异常类

---
+++ src/main/java/com/biostime/finagle/exception/ServiceNotFoundException.java
@@ -0,0 +1,27 @@
+package com.biostime.finagle.exception;
+
+/**
+ * @author Dylan
+ * @date 2016/9/1.
+ */
+public class ServiceNotFoundException extends RuntimeException {
+
+    public ServiceNotFoundException() {
+    }
+
+    public ServiceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ServiceNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ServiceNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    public ServiceNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
